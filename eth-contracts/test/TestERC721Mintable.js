1ada531fa9b5a7137d3aa036ecf2b8c30c503055
git-sid-go <siddhuvaradan@gmail.com>
2021-11-06 12:31:00 +0530

    feat: meet requirements in ERC721Mintable along with tests

--- eth-contracts/test/TestERC721Mintable.js
+++ eth-contracts/test/TestERC721Mintable.js
@@ -1,47 +1,82 @@
-var ERC721MintableComplete = artifacts.require('ERC721MintableComplete');
+var ERC721MintableComplete = artifacts.require("LiveSpaceERC721Token");
 
-contract('TestERC721Mintable', accounts => {
+contract("TestERC721Mintable", (accounts) => {
+  const account_one = accounts[0];
+  const account_two = accounts[1];
+  const account_three = accounts[2];
 
-    const account_one = accounts[0];
-    const account_two = accounts[1];
+  describe("match erc721 spec", function () {
+    beforeEach(async function () {
+      this.contract = await ERC721MintableComplete.new({ from: account_one });
 
-    describe('match erc721 spec', function () {
-        beforeEach(async function () { 
-            this.contract = await ERC721MintableComplete.new({from: account_one});
-
-            // TODO: mint multiple tokens
-        })
-
-        it('should return total supply', async function () { 
-            
-        })
-
-        it('should get token balance', async function () { 
-            
-        })
-
-        // token uri should be complete i.e: https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/1
-        it('should return token uri', async function () { 
-            
-        })
-
-        it('should transfer token from one owner to another', async function () { 
-            
-        })
+      // mint multiple tokens
+      await this.contract.mint(account_two, 21);
+      await this.contract.mint(account_two, 22);
+      await this.contract.mint(account_two, 23);
+      await this.contract.mint(account_two, 24);
+      await this.contract.mint(account_two, 25);
+      await this.contract.mint(account_two, 26);
+      await this.contract.mint(account_two, 27);
+      await this.contract.mint(account_two, 28);
+      await this.contract.mint(account_two, 29);
+      await this.contract.mint(account_two, 30);
+      await this.contract.mint(account_three, 31);
+      await this.contract.mint(account_three, 32);
+      await this.contract.mint(account_three, 33);
+      await this.contract.mint(account_three, 34);
+      await this.contract.mint(account_three, 35);
+      await this.contract.mint(account_three, 36);
+      await this.contract.mint(account_three, 37);
+      await this.contract.mint(account_three, 38);
+      await this.contract.mint(account_three, 39);
+      await this.contract.mint(account_three, 40);
     });
 
-    describe('have ownership properties', function () {
-        beforeEach(async function () { 
-            this.contract = await ERC721MintableComplete.new({from: account_one});
-        })
-
-        it('should fail when minting when address is not contract owner', async function () { 
-            
-        })
-
-        it('should return contract owner', async function () { 
-            
-        })
-
+    it("should return total supply", async function () {
+      const supply = await this.contract.totalSupply();
+      assert.equal(supply, 20, "Incorrect total suppy");
     });
-})
+
+    it("should get token balance", async function () {
+      const accountThreeBalance = await this.contract.balanceOf(account_three);
+      assert.equal(accountThreeBalance, 10, "Incorrect account balance");
+    });
+
+    // token uri should be complete i.e: https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/1
+    it("should return token uri", async function () {
+      const tokenURI = await this.contract.tokenURI(21);
+      assert.equal(
+        tokenURI,
+        "https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/21",
+        "Incorrect token uri"
+      );
+    });
+
+    it("should transfer token from one owner to another", async function () {
+      await this.contract.transferFrom(account_two, account_three, 29, {
+        from: account_two,
+      });
+      const newOwner = await this.contract.ownerOf(29);
+      assert.equal(newOwner, account_three, "Incorrect new owner");
+    });
+  });
+
+  describe("have ownership properties", function () {
+    beforeEach(async function () {
+      this.contract = await ERC721MintableComplete.new({ from: account_one });
+    });
+
+    it("should fail when minting when address is not contract owner", async function () {
+      await expectThrow(
+        this.contract.mint(account_two, 20, {
+          from: account_two,
+        })
+      );
+    });
+
+    it("should return contract owner", async function () {
+      const owner = await this.contract.owner();
+      assert.equal(owner, account_one, "Incorrect contract owner");
+    });
+  });
+});
